86b3fe05e3a82aa0eb50d5125d0ebfa1d1338012
Jérémie Roquet <jroquet@arkanosis.net>
2017-05-13 03:26:34 +0200

    Add command line parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
+extern crate docopt;
+extern crate rustc_serialize;
 
+const USAGE: &'static str = "
+Usage: microstatus <working-directory>
+       microstatus -h | --help
+       microstatus --version
+
+Arguments:
+    working_directory working directory (file storage)
+
+Options:
+    -h, --help  Show this screen.
+    --version   Show version.
+";
+
+#[derive(RustcDecodable)]
+struct Args {
+    arg_working_directory: String,
+    flag_version: bool,
+}
+
+fn main() {
+    let args: Args =
+        docopt::Docopt::new(USAGE)
+            .and_then(|docopts|
+                docopts.argv(std::env::args().into_iter())
+                   .decode()
+            )
+            .unwrap_or_else(|error|
+                error.exit()
+            );
+
+    if args.flag_version {
+        println!("microstatus v{}", option_env!("CARGO_PKG_VERSION").unwrap_or("unknown"));
+    } else {
+        println!("Working directory: {}", args.arg_working_directory);
+    }
 }
